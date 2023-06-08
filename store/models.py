5dd1643549fde26e4a61d6c8e8bb16a8d77f4978
Alessandro Capialbi <capialbi.alessandro@studenti-ittfedifermi.edu.it>
2023-06-08 15:10:39 +0200

    adding logic to cart.html in order to see quantity and total price of an order

--- store/models.py
+++ store/models.py
@@ -44,6 +44,18 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
 
 # OrderItem represents every item listed in our cart. There is a One-to-Many relationship between Order and
 # OrderItem. Also with Product.
@@ -52,6 +64,10 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        return self.quantity * self.product.price
 
 
 class ShippingAddress(models.Model):
