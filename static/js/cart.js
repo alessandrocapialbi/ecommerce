99845b3784d5cbf0bbf924b1f3f4332a06ce19e7
Alessandro Capialbi <capialbi.alessandro@studenti-ittfedifermi.edu.it>
2023-06-08 17:04:24 +0200

    adding fetching, sending data with POST and promises.

--- static/js/cart.js
+++ static/js/cart.js
@@ -17,4 +17,26 @@ for (i = 0; i < updateBtns.length; i++) {
 
 function updateUserOrder(productId, action) {
     console.log("User is authenticated, sending data!")
+    var url = '/update_item/'
+
+    //Fetch API lets us send data to the view.
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({'productId': productId, 'action': action})
+    })
+
+        .then((response) => {
+            return response.json()
+        })
+
+        .then((data) => {
+            console.log('data', data)
+        })
+
+
 }
