926eb44cda21522804167f403527c0c2180323c3
Alessandro Capialbi <capialbi.alessandro@studenti-ittfedifermi.edu.it>
2023-06-11 17:04:50 +0200

    final commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-^^*v8q+(-syei(4ti73)msbir@0tbwj@86n32y1@#orufkkquh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
