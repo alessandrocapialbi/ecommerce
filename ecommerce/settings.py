aa0d09c81da65fd8caae3e9ae1cb447473e76cf8
Alessandro Capialbi <capialbi.alessandro@studenti-ittfedifermi.edu.it>
2023-06-11 17:59:42 +0200

    final commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -50,6 +50,9 @@ MIDDLEWARE = [
 ]
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
