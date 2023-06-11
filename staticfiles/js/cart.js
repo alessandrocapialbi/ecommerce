a81b9741d98f574ce3df8543f0176d2cda58a7f8
Alessandro Capialbi <capialbi.alessandro@studenti-ittfedifermi.edu.it>
2023-06-11 17:28:16 +0200

    final commit

---
+++ staticfiles/js/cart.js
@@ -0,0 +1,66 @@
+var updateBtns = document.getElementsByClassName('update-cart')
+
+// When the user add a product we get productId and action in order to update the cart section
+for (i = 0; i < updateBtns.length; i++) {
+    updateBtns[i].addEventListener('click', function () {
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:', productId, 'Action:', action)
+        console.log('User:', user)
+
+        if (user === "AnonymousUser") {
+            addCookieItem(productId, action)
+        } else {
+            updateUserOrder(productId, action)
+        }
+    })
+}
+
+function addCookieItem(productId, action) {
+    console.log("Not authenticated")
+    if (action == 'add') {
+        if (cart[productId] == undefined) {
+            cart[productId] = {'quantity': 1}
+        } else {
+            cart[productId]['quantity'] += 1
+        }
+    }
+
+    if (action == 'remove') {
+        cart[productId]['quantity'] -= 1
+        if (cart[productId]['quantity'] <= 0) {
+            console.log('Remove item')
+            delete cart[productId]
+        }
+    }
+    console.log('Cart', cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+    location.reload()
+}
+
+function updateUserOrder(productId, action) {
+    console.log("User is authenticated, sending data!")
+    var url = '/update_item/'
+
+    // Fetch API lets us send data to the view.
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken,
+        },
+        body: JSON.stringify({'productId': productId, 'action': action})
+    })
+
+        .then((response) => {
+            return response.json()
+        })
+
+        .then((data) => {
+            console.log('data', data)
+            location.reload()
+        })
+
+
+}
