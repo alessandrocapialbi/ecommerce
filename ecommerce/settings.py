b7477cb2694d74fd138feee382f50b13f97d5ef9
Alessandro Capialbi <capialbi.alessandro@studenti-ittfedifermi.edu.it>
2023-06-11 16:22:21 +0200

    setting the project to deploy in railway

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-^^*v8q+(-syei(4ti73)msbir@0tbwj@86n32y1@#orufkkquh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store.apps.StoreConfig',
-    'whitenoise.runserver_nostatic',
+
 ]
 
 MIDDLEWARE = [
@@ -47,8 +47,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
@@ -115,10 +113,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 MEDIA_URL = '/images/'
 
